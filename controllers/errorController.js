3d5a58b4627bff2fb0f6ab55a9e6ff21aaa71128
shresthamall <shresthaubc@gmail.com>
2024-09-19 04:26:46 +0530

    Implemented error handling for dev and prod environments

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -2,11 +2,33 @@
 Global error handling function for the app
 Is called whenever next(err) is called anywhere/anytime during a process
 */
-module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
-  err.status = err.status || 'error';
+const { StatusCodes } = require('http-status-codes');
+
+const sendErrorDev = (res, err) => {
   res.status(err.statusCode).json({
     status: err.status,
     message: err.message,
   });
+};
+
+const sendErrProd = (res, err) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+  res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ status: 'error', message: 'Uh Oh! Something went wrong!' });
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  err.status = err.status || 'error';
+  // Operational, trusted error: send message to client
+  if (process.env.NODE_ENV === 'development') return sendErrorDev(res, err);
+
+  // Programming or other unknown error: don't leak error details
+  if (process.env.NODE_ENV === 'production') return sendErrProd(res, err);
 };
